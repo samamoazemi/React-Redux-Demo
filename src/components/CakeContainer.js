ec237a078c7a24e18cef83d5417a6c8a7cc0c7d2
samamoazemi <moazemisama@gmail.com>
2022-01-28 02:35:20 -0800

    Add redux to react

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,10 @@
+const CakeContainer = () => {
+    return ( 
+        <div>
+            <h2>number of cakes -</h2>
+            <button>buy cake</button>
+        </div>
+     );
+}
+ 
+export default CakeContainer;
